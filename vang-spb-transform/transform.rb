a6df456a0bec332a7e97469bdf6569a2e761f4f8
Carl Youngblood <carl@youngbloods.org>
2011-11-17 16:45:05 +0100

    Updated transform script to work with 1.9.3

--- vang-spb-transform/transform.rb
+++ vang-spb-transform/transform.rb
@@ -3,15 +3,22 @@ require 'csv'
 raise "Usage: ruby transform.rb <filename>" unless ARGV.first
 filename = ARGV.first
 raise "File not found: #{filename}" unless File.exists?(filename)
-transactions = CSV.read(ARGV.first)
+
+content = ''
+File.open(ARGV.first, 'r:iso-8859-1:utf-8') do |f|
+  content = f.read
+end
+content.gsub!(';', ',')
+
+transactions = CSV.parse(content)
 CSV.open("#{File.basename(filename, '.csv')}_new.csv", 'w') do |csv|
   transactions.each do |line|
     [0, 1].each do |index|
       line[index].gsub!('-', '.')
     end
     [3, 4].each do |index|
-      line[index].gsub!(',', '')
-      line[index].sub!('.', ',')
+      line[index].gsub!('.', '')
+#      line[index].sub!(',', '.')
     end
     csv << line
   end
